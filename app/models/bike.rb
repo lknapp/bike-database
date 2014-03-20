a33cc4423ffecb7c19422d933878006407e1f424
loos <louisknapp@gmail.com>
2014-03-20 17:07:30 -0500

    add some validations

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,9 @@
 class Bike < ActiveRecord::Base
+  validates :log_number, presence: true
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :bike_type, presence: true
+  validates :color, presence: true
+  validates :frame_size, presence: true
+  validates :serial_number, presence: true
 end
