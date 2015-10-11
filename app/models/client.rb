3cc429f2e6965138f672835be3aced2ac8ca549b
Louis Knapp <louisknapp@gmail.com>
2015-10-11 13:51:24 -0500

    lk | removes clients who have been assigned bikes from waiting list

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,7 +8,8 @@ class Client < ActiveRecord::Base
     clients = Client.all
     non_voided_clients = clients.select{|client| !client.application_voided}
     waiting_list_with_null_application_dates = non_voided_clients.select{|client| !client.completion_date}
-    waiting_list = waiting_list_with_null_application_dates.select{|client| !client.application_date.nil?}
+    clients_with_bikes = Client.all.select{|c| !c.bike_id.nil?}
+    waiting_list = waiting_list_with_null_application_dates.select{|client| !client.application_date.nil?} - clients_with_bikes
     waiting_list.sort_by!{|client| client.application_date}
   end
 
