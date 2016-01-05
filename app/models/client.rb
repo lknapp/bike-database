3d5b437832c69e6e940346527b90e3ee74240c54
Louis Knapp <him@louisknapp.com>
2016-01-05 14:16:24 -0600

    lk | shows bikes in client wait list

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,10 +6,9 @@ class Client < ActiveRecord::Base
 
   def self.waiting_list
     clients = Client.all
-    non_voided_clients = clients.select{|client| !client.application_voided}
-    waiting_list_with_null_application_dates = non_voided_clients.select{|client| !client.pickup_date}
-    clients_with_bikes = Client.all.select{|c| !c.bike_id.nil?}
-    waiting_list = waiting_list_with_null_application_dates.select{|client| !client.application_date.nil?} - clients_with_bikes
+    nonvoided_clients = clients.select{|client| !client.application_voided}
+    active_nonvoided_clients = nonvoided_clients.select{|client| !client.pickup_date}
+    waiting_list = active_nonvoided_clients.select{|client| !client.application_date.nil?}
     waiting_list.sort_by!{|client| client.application_date}
   end
 
