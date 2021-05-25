d0a4ba7ca2c120f81e0089f1ca78268548ff5731
Louis Knapp <louis.knapp@grainger.com>
2021-05-25 15:05:41 -0500

    lk | stop using coffescript

---
+++ app/assets/javascripts/bikes.js
@@ -0,0 +1,2 @@
+const theUsualRepairs = "- Trued Front & Rear Wheels\n- Adjusted Front & Rear Gears\n- Adjusted Front & Rear Brakes";
+$(document).on('click', '#the-usual',  () => $("#bike_work_done").val(theUsualRepairs));
